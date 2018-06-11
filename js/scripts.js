c778ecb35e30cd79cd25e60559937c04c75bf520
Kyle Parsons <23gdsm@gmail.com>
2018-06-11 01:38:32 -0700

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,29 @@
 
 function range(start, end) {
     var start = 0
+    var boop = "Boop!"
     var end = $('#numberInput').val();
     var result = [];
     for (var x = 1; x <= end ; x++) {
+      if (x === 0)  {
+        result.push("Boop!")
+      } else if (x === 10) {
+        result.push("Boop!")
+      } else if (x === 11) {
+        result.push("Boop!")
+      } else if (x%3 === 0) {
+        result.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (x%10 === 0) {
+        result.push("Beep!")
+      } else if (x%10 === 1) {
+        result.push("Boop!")
+      } else {
         result.push(x);
+      }
+
     }
     return result;
-} console.log(range);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  };
 
 
 
@@ -37,5 +35,5 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $('#numberInput').val();
     $('#result').text(range);
-  });
+  })
 });
