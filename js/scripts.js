d2ee77db86a413dd0bf891efedc85d08b80076b5
Kyle Parsons <23gdsm@gmail.com>
2018-06-17 22:26:56 -0700

    fix loop to ouput correct values and fix seperation of logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
 //business logic
-
-function range(start, end) {
-    var start = 0
-    var boop = "Boop!"
-    var end = $('#numberInput').val();
+function range(input) {
     var result = [];
-    for (var x = 1; x <= end ; x++) {
-      if (x === 0)  {
-        result.push("Boop!")
-      } else if (x === 10) {
-        result.push("Boop!")
-      } else if (x === 11) {
-        result.push("Boop!")
-      } else if (x%3 === 0) {
+    for (var x = 1; x <= input ; x++) {
+      var stringx = String(x)
+      if (x%3 == 0) {
         result.push("I'm sorry, Dave. I'm afraid I can't do that.")
-      } else if (x%10 === 0) {
-        result.push("Beep!")
-      } else if (x%10 === 1) {
+      }else if (stringx.startsWith(1))  {
         result.push("Boop!")
-      } else {
+      } else if (stringx.includes(0)) {
+        result.push("Beep")
+      }else{
         result.push(x);
       }
 
@@ -34,6 +25,6 @@ $(document).ready(function() {
   $("form#range").submit(function(event) {
     event.preventDefault();
     var input = $('#numberInput').val();
-    $('#result').text(range);
+    $('#result').text(range(input));
   })
 });
